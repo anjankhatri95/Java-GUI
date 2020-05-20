e48790c3143a52b1d25c20f3206e98013a1d62b3
Anjan <anjankhatri95@gmail.com>
2020-05-19 21:32:38 -0400

    exception of gas station file

---
+++ AmountException.java
@@ -0,0 +1,8 @@
+public class AmountException extends Exception{
+  public AmountException(){
+   super("Error: Amount is required !!!"); 
+  }
+  public AmountException(String message){
+    super(message); 
+  }
+}
